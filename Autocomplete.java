521f1e894a55edfdaff57edbbc98df75a7ca6424
James Gaskins <jenguskhan@gmail.com>
2020-03-15 17:37:53 -0400

    #

---
+++ Autocomplete.java
@@ -0,0 +1,94 @@
+package edu.umb.cs210.p3;
+
+import stdlib.In;
+import stdlib.StdIn;
+import stdlib.StdOut;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+// A data type that provides autocomplete functionality for a given set of 
+// string and weights, using Term and BinarySearchDeluxe. 
+public class Autocomplete {
+    private Term[] terms;   // The set of terms
+
+    // Initialize the data structure from the given array of terms.
+    public Autocomplete(Term[] terms) {
+        if (terms == null) {
+            throw new NullPointerException();
+        }
+        this.terms = new Term[terms.length];
+        for (int i = 0; i < terms.length; i++) {
+            this.terms[i] = terms[i];
+        }
+        Arrays.sort(this.terms);
+    }
+
+    // All terms that start with the given prefix, in descending order of
+    // weight.
+    public Term[] allMatches(String prefix) {
+        if (prefix == null) {
+            throw new NullPointerException();
+        }
+        Term termz = new Term(prefix);
+        Comparator<Term> prefixOrder = Term.byPrefixOrder(prefix.length());
+        int i = BinarySearchDeluxe.firstIndexOf(terms, termz, prefixOrder);
+        Term[] matches = new Term[numberOfMatches(prefix)];
+        for (int j = 0; j < matches.length; j++) {
+            matches[j] = terms[i + j];
+        }
+        Arrays.sort(matches, Term.byReverseWeightOrder());
+        return matches;
+    }
+
+    // The number of terms that start with the given prefix.
+    public int numberOfMatches(String prefix) {
+        if (prefix == null) {
+            throw new NullPointerException();
+        }
+        Term termz = new Term(prefix);
+        Comparator<Term> prefixOrder = Term.byPrefixOrder(prefix.length());
+        int i = BinarySearchDeluxe.firstIndexOf(terms, termz, prefixOrder);
+        if (i == -1) {
+            return 0;
+        }
+        int j = BinarySearchDeluxe.lastIndexOf(terms, termz, prefixOrder);
+        return j - i + 1;
+    }
+
+    // Entry point. [DO NOT EDIT]
+    public static void main(String[] args) {
+        String filename = args[0];
+        In in = new In(filename);
+        int N = in.readInt();
+        Term[] terms = new Term[N];
+        for (int i = 0; i < N; i++) {
+            long weight = in.readLong();
+            in.readChar();
+            String query = in.readLine();
+            terms[i] = new Term(query.trim(), weight);
+        }
+        int k = Integer.parseInt(args[1]);
+        Autocomplete autocomplete = new Autocomplete(terms);
+        StdOut.print("Enter a prefix: ");
+        boolean firstLoop = true;
+        while (StdIn.hasNextLine()) {
+            String prefix = StdIn.readLine();
+            Term[] results = autocomplete.allMatches(prefix);
+            String msg = " matches, in descending order by weight:";
+            if (results.length == 0) msg = "No matches";
+            else if (results.length > k) msg = "First " + k + msg;
+            else msg = "All" + msg;
+            StdOut.printf("%s\n", msg);
+            for (int i = 0; i < Math.min(k, results.length); i++) {
+                StdOut.println(results[i]);
+            }
+            StdOut.print("\nEnter a prefix, or press <ctrl-d> to quit : ");
+            if (firstLoop) {
+                StdOut.print("\n(Unless you're using IntelliJ to run. Then the "
+                        + "stop button will quit)\nThat prefix, please: ");
+                firstLoop = false;
+            }
+        }
+    }
+}
---
+++ Term.java
@@ -0,0 +1,102 @@
+package edu.umb.cs210.p3;
+
+import stdlib.In;
+import stdlib.StdOut;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+// An immutable data type that represents an autocomplete term: a query string 
+// and an associated real-valued weight.
+public class Term implements Comparable<Term> {
+    String query; // The natural order
+    long weight; // The alternate order
+
+    // Construct a term given the associated query string, having weight 0.
+    public Term(String query) {
+        this.query = query;
+        weight = 0;
+
+    }
+
+    // Construct a term given the associated query string and weight.
+    public Term(String query, long weight) {
+        if (query == null) { throw new NullPointerException(); }
+        if (weight < 0) { throw new IllegalArgumentException(); }
+        this.query = query;
+        this.weight = weight;
+    }
+
+    // Compare this term to that in lexicographic order by query and
+    // return a negative, zero, or positive integer based on whether this
+    // term is smaller, equal to, or larger than that term.
+    public int compareTo(Term that) {
+        return this.query.compareTo(that.query);
+    }
+
+    // A reverse-weight comparator.
+    public static Comparator<Term> byReverseWeightOrder() {
+        return new ReverseWeightOrder();
+    }
+
+    // Helper reverse-weight comparator.
+    private static class ReverseWeightOrder implements Comparator<Term> {
+        public int compare(Term v, Term w) {
+            return Long.compare(w.weight, v.weight);
+        }
+    }
+
+    // A prefix-order comparator.
+    public static Comparator<Term> byPrefixOrder(int r) {
+        return new PrefixOrder(r);
+    }
+
+    // Helper prefix-order comparator.
+    private static class PrefixOrder implements Comparator<Term> {
+        int r; // length of query
+
+        // Construct a new PrefixOrder object
+        PrefixOrder(int r) {
+            if (r < 0) { throw new IllegalArgumentException(); }
+            this.r = r;
+        }
+
+        public int compare(Term v, Term w) {
+            int lengthOfv = Math.min(r, v.query.length());
+            int lengthOfw = Math.min(r, w.query.length());
+            String a = v.query.substring(0, lengthOfv);
+            String b = w.query.substring(0, lengthOfw);
+            return a.compareTo(b);
+        }
+    }
+
+    // A string representation of this term.
+    public String toString() {
+        return weight + "\t" + query;
+    }
+
+    // Test client. [DO NOT EDIT]
+    public static void main(String[] args) {
+        String filename = args[0];
+        int k = Integer.parseInt(args[1]);
+        In in = new In(filename);
+        int N = in.readInt();
+        Term[] terms = new Term[N];
+        for (int i = 0; i < N; i++) {
+            long weight = in.readLong();
+            in.readChar();
+            String query = in.readLine();
+            terms[i] = new Term(query.trim(), weight);
+        }
+        StdOut.printf("Top %d by lexicographic order:\n", k);
+        Arrays.sort(terms);
+        for (int i = 0; i < k; i++) {
+            StdOut.println(terms[i]);
+        }
+        StdOut.printf("Top %d by reverse-weight order:\n", k);
+        Arrays.sort(terms, Term.byReverseWeightOrder());
+        for (int i = 0; i < k; i++) {
+            StdOut.println(terms[i]);
+        }
+    }
+}
